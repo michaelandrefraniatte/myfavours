ff475645fd12fba4a9526c417e41c57a6da00eba
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2023-08-07 09:23:17 +0200

    Change logo

--- Src/my-favours/my-favours/Form1.Designer.cs
+++ Src/my-favours/my-favours/Form1.Designer.cs
@@ -41,11 +41,12 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(1268, 622);
+            this.ClientSize = new System.Drawing.Size(1691, 766);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "my-favours";
