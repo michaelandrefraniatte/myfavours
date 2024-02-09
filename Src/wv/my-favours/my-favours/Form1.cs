c6fb1d1d7ed1ebe210a9902dec797be66d6df60c
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-02-10 00:00:56 +0100

    Update about

--- Src/wv/my-favours/my-favours/Form1.cs
+++ Src/wv/my-favours/my-favours/Form1.cs
@@ -160,7 +160,7 @@ namespace my_favours
         {
             if (keyData == Keys.F1)
             {
-                const string message = "• Author: Michaël André Franiatte.\n\r\n\r• Contact: michael.franiatte@gmail.com.\n\r\n\r• Publisher: https://github.com/michaelandrefraniatte.\n\r\n\r• Copyrights: All rights reserved, no permissions granted.\n\r\n\r• License: Not open source.";
+                const string message = "• Author: Michaël André Franiatte.\n\r\n\r• Contact: michael.franiatte@gmail.com.\n\r\n\r• Publisher: https://github.com/michaelandrefraniatte.\n\r\n\r• Copyrights: All rights reserved, no permissions granted.\n\r\n\r• License: Not open source, not free of charge to use.";
                 const string caption = "About";
                 MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
